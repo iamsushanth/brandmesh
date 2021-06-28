a6e09364088e8fbb41d4bbea37eda21e6a08c67c
Sushanth Kurdekar <su@Sushanths-MacBook-Air.local>
2021-06-28 21:43:22 +0530

    first9

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,9 +7,9 @@ const Card = props => {
   const { name, slug, summary, thumbnail } = props
 
   return (
-    <div className="bg-white h-5/6 shadow-sm rounded-md overflow-hidden group">
+    <div className="bg-white h-full shadow-sm rounded-md overflow-hidden group">
       <Link to={`/${slug}`}>
-        <div className="group-hover:opacity-75 transition duration-150 ease-in-out">
+        <div className="group-hover:opacity-75 h-5/6 transition duration-150 ease-in-out">
           <Img fluid={thumbnail.localFile.childImageSharp.fluid} alt={name} />
         </div>
         <div className="p-4 sm:p-5">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,18 +2,16 @@ import React from "react"
 
 const Hero = () => (
   <div className="container py-12 lg:pb-16">
-    <h2 className="text-3xl sm:text-4xl font-extrabold leading-tight tracking-tight text-gray-900">
+    <h2 className="text-3xl sm:text-4xl font-extrabold leading-tight tracking-tight text-gray-900 flex flex-wrap content-center">
       Hello, Welcome to Brand Mesh{" "}
       <span role="img" aria-label="waving hand">
-        👋
+        👋 &nbsp;&nbsp;
       </span>
       <br />
-    </h2>
-    <h3>
-    <span className="text-blue-600 text-3xl">
+      <span className="text-blue-600 text-3xl">
         Let’s Grow Your Brand To The Next Level.
       </span>
-    </h3>
+    </h2>
   </div>
 )
 
